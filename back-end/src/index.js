79b447c62601dde5206e740e93d868ed7acfaf22
Dev_Faisal <dfaisal59@gmail.com>
2024-05-09 21:23:14 +0530

    chore: Update demo data and add password hashing

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -7,7 +7,8 @@ import cartRouter from "../routes/cartRoute.js";
 import productModel from "../models/productModel.js";
 import cartModel from "../models/CartMode.js";
 import userModel from "../models/userModel.js";
-import demoData from "../demoData.json" with { "type": "json" };;
+import demoData from "../demoData.json" with { "type": "json" };
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
@@ -21,45 +22,42 @@ app.use("/api/users", router);
 app.use("/api/products", ProductRouter);
 app.use("/api/cart", cartRouter);
 
-// app.post("/deleteDB", (req, res) => {
-//   cartModel.deleteMany({}).then(() => {
-//     res.status(200).json({ message: "Database deleted" });
-//   });
-// });
+app.post("/deleteDB", (req, res) => {
+  productModel.deleteMany({}).then(() => {
+    res.status(200).json({ message: "Database deleted" });
+  });
+});
 
-console.log(demoData.products);
 app.post("/demo", (req, res) => {
   try {
     productModel.insertMany(demoData.products).then(() => {
       console.log("Data inserted");
     });
-    userModel.insertMany(demoData.users).then(() => {
+    const demoUsersPasswordHashed = demoData.users.map((user) => { 
+      return {
+        ...user,
+        password: bcrypt.hashSync(user.password, 10),
+      };
+    });
+    console.log(demoUsersPasswordHashed);
+    userModel.insertMany(demoUsersPasswordHashed).then(() => {
       console.log("Data inserted");
     });
-    res.send("Data inserted");
+    res.status(200).json({ message: "Demo data inserted" });
   } catch (error) {
     console.log(error);
   }
 });
 
 app.listen(port, () => {
-  
   console.log(`Server is running on port ${port}`);
-  connect().then(async () => {
-    console.log("Database connected");
-    if ((await userModel.find()).length === 0) {
-      await userModel.insertMany(demoData.users).then(() => {
-        console.log("Data inserted");
-      });
-      productModel.insertMany(demoData.products).then(() => {
-        console.log("Data inserted");
-      });
-    }
-    return;
-  })
-    .catch((error) => {
-      console.log("Database not connected");
-      console.log(error);
-     });
+  connect().then(async  () => { 
+    if( await userModel.find({}).length === 0){
+    fetch("http://localhost:3000/demo", {
+      method: "POST",
+    });
+  }
+  });
+  
 });
 
